a60be7b7e82057350e9948effde03064cf7c11be
RaykoEZ <onion55zh@gmail.com>
2017-11-11 00:34:29 +0000

    fixing PxrGlass issues

--- Struff.py
+++ Struff.py
@@ -1,4 +1,3 @@
-
 #setenv PYTHONPATH RMANTREE/bin  
 #Win: Go to Control Panel>>Systems>>Advanced>>Environment Vars, 
 # edit/add PYTHONPATH variable...use %RMANTREE%/bin   So annoying... 
@@ -11,15 +10,35 @@ ri = prman.Ri() # create an instance of the RenderMan interface
 rendertarget = "SeriousStruff.rib"
 ri.Begin(rendertarget)  # set rendertarget to ri.RENDER to render pixels
 ri.Display("SeriousStruff.exr", "it", "rgba")
-
+ri.Hider("raytrace",{"int incremental": [1],"int maxsamples": [64],"int minsamples":[4]})
+ri.Integrator("PxrPathTracer","MyIntegrator",{"int numLightSamples":[4],"int numBxdfSamples": [4],"int numIndirectSamples": [1]})
 ri.Format(512,512,1)
 ri.Projection(ri.PERSPECTIVE, {ri.FOV: 45}) # standard Ri tokens are available
 ri.Translate(0,0,8)
 ri.Rotate(-90,0,0,1)
 ri.Rotate(60,0,1,0)
-#ri.Rotate(-90,1,0,0)
+#ri.Rotate(90,0,1,0)
+#ri.Rotate(90,1,0,0)
 
 ri.WorldBegin()
+
+#domelight
+ri.AttributeBegin()
+ri.TransformBegin()
+
+#ri.Light("PxrDomeLight","myLight",{"float exposure":[0]},
+#{"string lightColorMap": ["sky_env.tx"]})
+ri.Translate (0.0,0.0,-5.0)
+ri.Light("PxrSphereLight", "pointLight1",{"float intensity":[50]})
+
+ri.TransformEnd()
+ri.AttributeEnd()
+
+ri.AttributeBegin()
+ri.TransformBegin()
+
+
+ri.Translate(0,0,-1)
 
 #Lens Frame
 ri.TransformBegin()
@@ -27,7 +46,6 @@ ri.AttributeBegin()
 
 ri.Bxdf("PxrDisney","forFrame",
 {"color baseColor": [0.0,0.3,1.0]}
-
 )
 ri.Cylinder(1.0,0.6,0.8,360)
 
@@ -38,8 +56,13 @@ ri.TransformEnd()
 ri.TransformBegin()
 ri.AttributeBegin()
 
+ri.Bxdf("PxrGlass","Len1",
+{"float ior":1.47,"color transmissionColor": [0,1,0],"color reflectionColor": [0,1,0]})
+
+#ri.Bxdf("PxrDisney","Lens1Dis",{"color baseColor": [1,1,1]},{"float roughness":0})
+
 ri.Translate(0.0,0.0,0.7)
-ri.Scale(1.95,1.95,0.3)
+ri.Scale(2.0,2.0,0.3)
 ri.Sphere(0.5,-1.0,0.0,360)
 
 ri.AttributeEnd()
@@ -49,14 +72,23 @@ ri.TransformEnd()
 ri.TransformBegin()
 ri.AttributeBegin()
 
+ri.Bxdf("PxrGlass","Len2",
+{"float ior":1.47,"color transmissionColor":[0,1,0],"color reflectionColor":[0,1,0]})
+
+#ri.Bxdf("PxrDisney","Lens2Dis",{"color baseColor": [1,1,0.5]},{"float roughness":0})
+
+
 ri.Translate(0.0,0.0,0.8)
-ri.Scale(1.95,1.95,0.3)
+ri.Scale(2.0,2.0,0.3)
 ri.Sphere(0.5,1.0,0.0,360)
 
 ri.AttributeEnd()
 ri.TransformEnd()
 
-#Handle
+
+
+
+#Handle----------------------------------------------------------------------------
 
 ri.AttributeBegin()
 ri.TransformBegin()
@@ -68,11 +100,56 @@ ri.Scale(0.6,0.6,8.0)
 ri.Cylinder(0.15,0.35,0.6,360)
 ri.TransformEnd()
 
+#Handle to frame----------------------------------
+ri.AttributeBegin()
 ri.TransformBegin()
+ri.Bxdf("PxrDisney","frameToHandle",{"color baseColor":[0.1,0.5,0.5],"float specular":[1.0]})
+
+ri.Scale(0.7,0.3,0.5)
+#ri.Rotate(0,0,0,1)
+
+ri.Translate(0,2.0,1.4)
+
+ri.Torus(1,0.25,90,-90, 45 )
 
 ri.TransformEnd()
+ri.AttributeEnd()
+#----------------------------------------------
+#cone at the end of handle
+ri.AttributeBegin()
+ri.TransformBegin()
+
+ri.Translate(2.14,2.14,0.69)
+ri.Rotate(90, 0,1,0)
+ri.Rotate(-45,1,0,0)
+ri.Scale(0.6,0.6,0.6)
+
+ri.Cone(0.09,0.15,360)
+
+ri.TransformEnd()
+ri.AttributeEnd()
 
 ri.AttributeEnd()
+
+ri.TransformEnd()
+ri.AttributeEnd()
+
+#----------------------
+#Patch for Table Surface
+ri.AttributeBegin()
+ri.TransformBegin()
+
+ri.Rotate(90, 0,0,1)
+
+ri.Translate(-10,-5,0.82)
+ri.Bxdf("PxrDisney","frameToHandle",{"color baseColor":[1.0,0.0,1.0]})
+ri.Patch("bilinear",{ri.P:[0,0,0,  20, 0 ,0,
+                       0, 20, 0,  20, 20, 0]})
+
+ri.TransformEnd()
+ri.AttributeEnd()
+
+
 
 ri.WorldEnd()
 ri.End()
