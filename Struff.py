c8db26beb599b7729f88f49e8d45830f0eca5858
RaykoEZ <onion55zh@gmail.com>
2017-11-13 11:05:02 +0000

    Some edits

--- Struff.py
+++ Struff.py
@@ -1,3 +1,4 @@
+
 #setenv PYTHONPATH RMANTREE/bin  
 #Win: Go to Control Panel>>Systems>>Advanced>>Environment Vars, 
 # edit/add PYTHONPATH variable...use %RMANTREE%/bin   So annoying... 
@@ -10,13 +11,13 @@ ri = prman.Ri() # create an instance of the RenderMan interface
 rendertarget = "SeriousStruff.rib"
 ri.Begin(rendertarget)  # set rendertarget to ri.RENDER to render pixels
 ri.Display("SeriousStruff.exr", "it", "rgba")
-ri.Hider("raytrace",{"int incremental": [1],"int maxsamples": [64],"int minsamples":[4]})
+ri.Hider("raytrace",{"int incremental": [1],"int maxsamples": [128],"int minsamples":[4]})
 ri.Integrator("PxrPathTracer","MyIntegrator",{"int numLightSamples":[4],"int numBxdfSamples": [4],"int numIndirectSamples": [1]})
 ri.Format(512,512,1)
 ri.Projection(ri.PERSPECTIVE, {ri.FOV: 45}) # standard Ri tokens are available
 ri.Translate(0,0,8)
 ri.Rotate(-90,0,0,1)
-ri.Rotate(45,0,1,0)
+ri.Rotate(60,0,1,0)
 #ri.Rotate(90,0,1,0)
 #ri.Rotate(90,1,0,0)
 
@@ -25,9 +26,10 @@ ri.WorldBegin()
 #domelight
 ri.AttributeBegin()
 ri.TransformBegin()
+ri.Rotate(180,1,0,0)
+#ri.Rotate(180,0,1,0)
 
-ri.Light("PxrDomeLight","myLight",{"float exposure":[0]},
-{"string lightColorMap": ["sky_env.tx"]})
+ri.Light("PxrDomeLight","myLight",{"float exposure":[-1],"string lightColorMap": ["room.tx"]})
 
 
 ri.TransformEnd()
@@ -46,10 +48,10 @@ ri.TransformBegin()
 ri.AttributeBegin()
 
 ri.Bxdf("PxrDisney","forFrame",
-{"color baseColor": [0.9,0.7,0.3],
-"float metallic":[0.667],
+{"color baseColor": [0.9,0.9,0.5],
+"float metallic":[1.0],
 "float specular": [0.5],
-"float roughness": [0.1],
+"float roughness": [0.3],
 "float clearcoat":[0.5]
 })
 ri.Cylinder(1.0,0.6,0.8,360)
@@ -84,8 +86,8 @@ ri.AttributeBegin()
 ri.TransformBegin()
 
 ri.Bxdf("PxrDisney","forHandle",
-{"color baseColor": [0.9,0.7,0.3],
-"float metallic":[0.667],
+{"color baseColor": [0.9,0.9,0.5],
+"float metallic":[1.0],
 "float specular": [0.5],
 "float roughness": [0.1],
 "float clearcoat":[0.5]
@@ -107,8 +109,8 @@ ri.AttributeBegin()
 ri.TransformBegin()
 
 ri.Bxdf("PxrDisney","forFrame1",
-{"color baseColor": [0.9,0.7,0.3],
-"float metallic":[0.667],
+{"color baseColor": [0.9,0.9,0.5],
+"float metallic":[1.0],
 "float specular": [0.5],
 "float roughness": [0.1],
 "float clearcoat":[0.5]
@@ -129,8 +131,8 @@ ri.AttributeBegin()
 ri.TransformBegin()
 
 ri.Bxdf("PxrDisney","forCone",
-{"color baseColor": [0.9,0.7,0.3],
-"float metallic":[0.667],
+{"color baseColor": [0.9,0.9,0.5],
+"float metallic":[1.0],
 "float specular": [0.5],
 "float roughness": [0.1],
 "float clearcoat":[0.5]
@@ -159,7 +161,7 @@ ri.TransformBegin()
 ri.Rotate(90, 0,0,1)
 
 ri.Translate(-10,-5,0.82)
-ri.Pattern("PxrTexture","patchTex",{"string filename":["Quartered_sepele_pxr128.tx"]})
+ri.Pattern("PxrTexture","patchTex",{"string filename":["wood.tx"]})
 ri.Bxdf("PxrDisney","frameToHandle",{"reference color baseColor":["patchTex:resultRGB"]})
 ri.Patch("bilinear",{ri.P:[0,0,0,  20, 0 ,0,
                        0, 20, 0,  20, 20, 0]})
